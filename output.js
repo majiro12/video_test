ff554985060177bbbcf4fdf212d597e3ccb427cf
majiro12 <119295032+majiro12@users.noreply.github.com>
2024-02-15 00:01:11 +0900

    Add files via upload

---
+++ output.js
@@ -0,0 +1,5 @@
+var url = "./test1_000000000000_keypoints.json";
+
+$.getJSON(url , (data) => {
+    console.log('data: ', data);
+})
--- server.js
+++ server.js
@@ -3,16 +3,21 @@
 //モジュール
 const express = require("express");
 const http = require("http");
+const path = require("path");
 
 //オブジェクト
 const app = express();
 const server = http.Server(app);
 
 //定数
-const PORT = process.env.PORT || 1337;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
 
 //公開フォルダの指定
-app.use(express.static(__dirname + "/public"));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
 //サーバの起動
 server.listen(
